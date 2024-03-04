4b12142b0d33e7910cccf0453aa7495d79525b90
ErickUEA <ea.pazc@uea.edu.ec>
2024-03-04 14:33:33 -0500

    Practicar la definición y uso de funciones en Python para calcular temperaturas promedio y utilizar la plataforma GitHub para compartir el desarrollo de código.
    Función en Python que calcule la temperatura promedio de una ciudad durante un período de tiempo. La función debe ser capaz de manejar datos de temperaturas de múltiples ciudades y semanas.
    Utilizar como base el ejemplo anterior, donde teniamos datos de 2 ciudades durante 3 semanas.
    Tu función debe recibir estos datos como parámetros y calcular la temperatura promedio de cada ciudad.

--- Registro de Temperaturas Diarias/arreglos_multidimensionales_con_bucles_anidados.py
+++ Registro de Temperaturas Diarias/arreglos_multidimensionales_con_bucles_anidados.py
@@ -4,7 +4,6 @@
 # Tercera dimensión: Días de la semana (7 días)
 # datos obtenidos de: https://shre.ink/rTgW
 def calcular_temperatura_promedio(ciudades, temperaturas):
-
     promedios_ciudades = []  # Lista para almacenar los promedios de temperatura por ciudad
 
     # Recorremos la lista de temperaturas
@@ -25,15 +24,15 @@ def calcular_temperatura_promedio(ciudades, temperaturas):
 
 ciudades = ["Ambato", "Tena"]
 temperaturas = [
-    [
+    [  # cuidad 1
         ["Lunes", 24, "Martes", 26, "Miércoles", 25, "Jueves", 23, "Viernes", 24, "Sábado", 25, "Domingo", 26],
         ["Lunes", 26, "Martes", 27, "Miércoles", 27, "Jueves", 31, "Viernes", 28, "Sábado", 16, "Domingo", 16],
         ["Lunes", 29, "Martes", 31, "Miércoles", 29, "Jueves", 29, "Viernes", 31, "Sábado", 29, "Domingo", 26]
     ],
-    [
-        ["Lunes", 29, "Martes",  31, "Miércoles", 29, "Jueves", 28, "Viernes", 27, "Sábado", 30, "Domingo", 31],
+    [  # cuidad 2
+        ["Lunes", 29, "Martes", 31, "Miércoles", 29, "Jueves", 28, "Viernes", 27, "Sábado", 30, "Domingo", 31],
         ["Lunes", 30, "Martes", 32, "Miércoles", 31, "Jueves", 30, "Viernes", 29, "Sábado", 29, "Domingo", 30],
-        ["Lunes", 31, "Martes",  29, "Miércoles", 30, "Jueves", 29, "Viernes", 30, "Sábado", 32, "Domingo", 28]
+        ["Lunes", 31, "Martes", 29, "Miércoles", 30, "Jueves", 29, "Viernes", 30, "Sábado", 32, "Domingo", 28]
     ]
 ]
 
