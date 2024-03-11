4b9c772464700a8377ea1ba313c6f711614ecba4
ErickUEA <ea.pazc@uea.edu.ec>
2024-03-11 12:55:43 -0500

    Este programa tiene una función llamada calcular_descuento que tome dos parámetros: el monto total de la compra y un valor predeterminado para el porcentaje de descuento (por ejemplo, 20% por defecto).
    La función calcula el descuento aplicando el porcentaje al monto total de la compra.
    La función devuelve el monto del descuento calculado.
    
    Llama a la función calcular_descuento al menos dos veces desde el programa principal.
    En una llamada, proporciona el monto total de la compra y, en la otra, proporciona tanto el monto total de la compra como el porcentaje de descuento.
    
    Muestra los resultados de las llamadas a la función, incluyendo el monto del descuento y el monto final a pagar después del descuento.

---
+++ Cálculo de Descuento en Compras/calcular-descuento.py
@@ -0,0 +1,26 @@
+def calcular_descuento(monto_t, porcentaje_des=20):
+    descuento = monto_t * (porcentaje_des / 100)
+    return descuento
+
+
+# Primera llamada
+compra_1 = int(input("Ingrese el valor de la compra 1: "))
+des_1 = calcular_descuento(compra_1)
+monto_final_1 = compra_1 - des_1
+print("\n┌-------------------------------------------------------------------┐")
+print(f"| Monto de compra 1: {compra_1}$                                          |")
+print(f"| Descuento aplicado a la compra 1: {des_1:.2f}$                        |")
+print(f"| Valor total al pagar de la compra 1: {monto_final_1:.2f}$                     |")
+print("└-------------------------------------------------------------------┘")
+
+# Segunda llamada
+compra_2 = int(input("\n Ingrese el valor de la compra 2: "))
+porcentaje_des = int(input("Ingrese el porcentaje del descuento: "))
+des_2 = calcular_descuento(compra_2, porcentaje_des)
+monto_final_2 = compra_2 - des_2
+print("\n┌-------------------------------------------------------------------┐")
+print(f"| Monto de compra 2: {compra_2}$                                          |")
+print(f"| Porcentaje de descuento a la compra 2: {porcentaje_des}%                        |")
+print(f"| Descuento aplicado a la compra 2: {des_2:.2f}$                        |")
+print(f"| Valor total al pagar de la compra 2: {monto_final_2:.2f}                      |")
+print("└-------------------------------------------------------------------┘")
