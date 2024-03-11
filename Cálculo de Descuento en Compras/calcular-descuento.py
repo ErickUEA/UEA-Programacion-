34c6a10688ab4702586fa9a2c235f1873bb62290
ErickUEA <ea.pazc@uea.edu.ec>
2024-03-11 14:35:04 -0500

    Este programa tiene una función llamada calcular_descuento que tome dos parámetros: el monto total de la compra y un valor predeterminado para el porcentaje de descuento (por ejemplo, 20% por defecto).
    La función calcula el descuento aplicando el porcentaje al monto total de la compra.
    La función devuelve el monto del descuento calculado.
    
    Llama a la función calcular_descuento al menos dos veces desde el programa principal.
    En una llamada, proporciona el monto total de la compra y, en la otra, proporciona tanto el monto total de la compra como el porcentaje de descuento.
    
    Muestra los resultados de las llamadas a la función, incluyendo el monto del descuento y el monto final a pagar después del descuento.

--- Cálculo de Descuento en Compras/calcular-descuento.py
+++ Cálculo de Descuento en Compras/calcular-descuento.py
@@ -8,6 +8,7 @@ def calcular_descuento(monto_t, porcentaje_des=20):
 compra_1 = int(input("Ingrese el valor de la compra 1: "))
 des_1 = calcular_descuento(compra_1)
 monto_final_1 = compra_1 - des_1
+# Aqui se imprime de manera bonita el resultado
 print("\n┌-------------------------------------------------------------------┐")
 print(f"| Monto de compra 1: {compra_1}$                                          |")
 print(f"| Descuento aplicado a la compra 1: {des_1:.2f}$                        |")
