decd53ed1564d19302d70433f660eb7a899d0717
ErickUEA <ea.pazc@uea.edu.ec>
2024-03-18 17:06:36 -0500

    Este programa: Tiene un diccionario llamado informacion_personal que contenga información ficticia sobre una persona, incluyendo al menos las claves "nombre", "edad", "ciudad" y "profesion". Accede al valor asociado con la clave "ciudad" y modifíca con una ciudad diferente. Agrega una nueva clave-valor al diccionario que represente la "profesion" de la persona. Verifica si la clave "teléfono" existe en el diccionario Si no existe, agréga un número de teléfono ficticio. Elimina la clave "edad" del diccionario, ya que esa información no es necesaria. Imprime el diccionario resultante después de realizar todas las operaciones.

---
+++ Trabajando con Diccionarios en Python/diccionario.py
@@ -0,0 +1,26 @@
+# Se crea un diccionario de nombre informacion_personal
+informacion_personal = {"nombre": "Patricia", "edad": 18, "cuidad": "Puyo"}
+# Imprime el diccionario original
+print("Diccionario original", informacion_personal)
+# Se cambia el valor de la clave "cuidad" por Ambato
+informacion_personal["cuidad"] = "Ambato"
+# Se imprime el diccionario con la ciudad cambiada
+print("\nCuidad Modificada: ", informacion_personal)
+# Se añade la clave profesion con un valor cualquiera
+informacion_personal["profesion"] = "Estudiante"
+# Se imprime la adición de la profesion
+print("\nProfresion añadida: ", informacion_personal)
+# Se verifica si el teléfono existe en el diccionario
+if "Teléfono" in informacion_personal:
+    print(informacion_personal)
+else:
+    # mensaje de que no existe el número de teléfono
+    print("\nNo existe numero de teléfono, añadiendo uno =)")
+    # Se añade la clave "Teléfono" con un valor cualquiera
+    informacion_personal["Teléfono"] = "0991278250"
+    # Se imprime el teléfono
+    print(informacion_personal)
+# Se borra la edad del diccionario
+del informacion_personal["edad"]
+# Se imprime el diccionario final con la edad eliminada
+print("\nEdad eliminada y Diccionario resultante: ", informacion_personal)
